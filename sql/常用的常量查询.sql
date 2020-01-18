af7b9f7e02434ff8dbbb67160334e942a01402b7
Pop <824173124@qq.com>
2020-01-18 22:09:50 +0800

    更新语句和查询语句的执行流程

---
+++ sql/常用的常量查询.sql
@@ -0,0 +1,18 @@
+INSERT INTO `常用的常量查询` VALUES ('wait_timeout', '28800');
+INSERT INTO `常用的常量查询` VALUES ('interactive_timeout', '28800');
+INSERT INTO `常用的常量查询` VALUES ('Threads_cached', '6');
+INSERT INTO `常用的常量查询` VALUES ('Threads_connected', '3');
+INSERT INTO `常用的常量查询` VALUES ('Threads_created', '9');
+INSERT INTO `常用的常量查询` VALUES ('Threads_running', '1');
+INSERT INTO `常用的常量查询` VALUES (16, 'root', 'localhost:50093', 'qingshan_mysql', 'Sleep', 42, '', NULL);
+INSERT INTO `常用的常量查询` VALUES (18, 'root', 'localhost:50204', 'qingshan_mysql', 'Sleep', 34, '', NULL);
+INSERT INTO `常用的常量查询` VALUES (19, 'root', 'localhost:50266', 'qingshan_mysql', 'Query', 0, 'starting', '-- 查看 MySQL 当前有多少个连接\r\n\r\nshow processlist');
+INSERT INTO `常用的常量查询` VALUES ('max_connections', '200');
+INSERT INTO `常用的常量查询` VALUES ('max_allowed_packet', '4194304');
+INSERT INTO `常用的常量查询` VALUES ('query_cache_limit', '1048576');
+INSERT INTO `常用的常量查询` VALUES ('query_cache_min_res_unit', '4096');
+INSERT INTO `常用的常量查询` VALUES ('query_cache_size', '1048576');
+INSERT INTO `常用的常量查询` VALUES ('query_cache_type', 'OFF');
+INSERT INTO `常用的常量查询` VALUES ('query_cache_wlock_invalidate', 'OFF');
+INSERT INTO `常用的常量查询` VALUES ('Last_query_cost', '0.000000');
+INSERT INTO `常用的常量查询` VALUES ('optimizer_trace', 'enabled=off,one_line=off');
